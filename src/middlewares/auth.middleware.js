9520b2665f294d01690e437b17f25fb7cda1b23b
hasmukhsingh9901 <hasmukhsingh90@gmail.com>
2025-06-22 06:28:18 +0530

    Update middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,34 +1,31 @@
-import User from "../models/user.model.js";
-import jwt from "jsonwebtoken";
-import { ACCESS_TOKEN_SECRET } from "../config/env.js";
-
-
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model.js';
+import { ACCESS_TOKEN_SECRET } from '../config/env.js';
 
 const authMiddleware = async (req, res, next) => {
-    const authHeader = req.header('Authorization');
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'No token provided' });
-    }
+  const authHeader = req.header('Authorization');
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
 
-    const token = authHeader.replace('Bearer ', '');
-    try {
-        const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET);
-        req.user = await User.findById(decoded.id).select('-password -refreshToken');
-        if (!req.user) {
-            return res.status(401).json({ message: 'User not found' });
-        }
-        next();
-    } catch (error) {
-        res.status(401).json({ message: 'Invalid token' });
+  const token = authHeader.replace('Bearer ', '');
+  try {
+    const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET);
+    req.user = await User.findById(decoded.id).select('-password -refreshToken');
+    if (!req.user) {
+      return res.status(401).json({ message: 'User not found' });
     }
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid token' });
+  }
 };
 
 const adminMiddleware = (req, res, next) => {
-    if (req.user.role !== 'admin') {
-        return res.status(403).json({ message: 'Admin access required' });
-    }
-    next();
+  if (req.user.role !== 'admin') {
+    return res.status(403).json({ message: 'Admin access required' });
+  }
+  next();
 };
 
 export { authMiddleware, adminMiddleware };
-
